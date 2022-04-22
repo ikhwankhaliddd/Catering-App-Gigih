7ce52001f4e8cce8b3848b212b097d98c8038d5c
Khaleed <ikhwankhalid@student.telkomuniversity.ac.id>
2022-04-22 19:27:58 +0700

    Fixed for deployment

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,3 @@
-//= link application.js
+//= link applicationn.js
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
--- app/assets/javascript/application.js
+++
@@ -1,7 +0,0 @@
-//= require jquery3
-// = require popper
-//= require bootstrap-sprockets
-//= require jquery
-//= require jquery_ujs
-import "popper"
-import "bootstrap"
