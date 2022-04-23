90828be93885627ae88c685b90de1b0e0517ee4a
Khaleed <ikhwankhalid@student.telkomuniversity.ac.id>
2022-04-23 08:16:58 +0700

    Add Edit Template & Edit Action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,20 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account was successfully updated"
+      redirect_to foods_path
+    else
+      render 'edit'
+    end
   end
 end
 
