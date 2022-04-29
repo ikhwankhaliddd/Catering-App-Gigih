08c11a9c64574559f3b34e2b46cdbbb16f2c3e35
Khaleed <ikhwankhalid@student.telkomuniversity.ac.id>
2022-04-30 02:03:16 +0700

    Add categories model and its unit test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
 
   def require_user
     if !logged_in?
-      flash[:alert] = "You have to log in first!"
+      flash[:danger] = "You have to log in first!"
       redirect_to login_path
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_25_043146) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_29_173606) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
